5909224e8b7e4bee1728dd3a0b2a41be0a4f7644
Nir <orfaly3c@gmail.com>
2019-11-27 09:07:19 +0200

    Update delete-files-modify-date.sh
    fix script

--- delete-files-modify-date.sh
+++ delete-files-modify-date.sh
@@ -1,2 +1,2 @@
-files=find . -newermt "09/09/2013 13:21:22" ! -newermt "09/09/2020 13:21:22"
+files=$(find . -newermt "09/09/2013 13:21:22" ! -newermt "09/09/2020 13:21:22") # format of mm/dd/YYYY HH:MM:SS
 /bin/bash delete-last-row-and-remove-extension-if-needed.sh $files
